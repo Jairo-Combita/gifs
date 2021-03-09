98fedad6df8b83489c930f2dffe51085c13e610e
jCombita <jairoalbertocombita@gmail.com>
2021-03-09 00:22:48 -0500

    Primer commit

---
+++ precache-manifest.5300685c40b8073351438bda744d2b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdcd07b1c54541cb87dada3c7ad4ab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "292250f1c92d8b99eac0",
+    "url": "/static/css/main.55835ceb.chunk.css"
+  },
+  {
+    "revision": "7e19193e9a1b175b825e",
+    "url": "/static/js/2.3d6af759.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d6af759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292250f1c92d8b99eac0",
+    "url": "/static/js/main.72f64f26.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
